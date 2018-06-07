bf1b4356f6dc101caffc71001619b6befb4b4027
Colin C <colin@mesosphere.com>
2018-06-07 16:01:05 -0700

    Do not write to /etc/

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -133,7 +133,8 @@ def main():
         jenkins_home_dir, 'jenkins.model.JenkinsLocationConfiguration.xml'),
         jenkins_root_url)
 
-    populate_known_hosts(ssh_known_hosts, '/etc/ssh/ssh_known_hosts')
+    populate_known_hosts(ssh_known_hosts,
+                         "{}/.ssh/ssh_known_hosts".format(jenkins_home_dir))
 
     # nginx changes here are really "run once". The context should never
     # change as long as a Jenkins instance is alive, since the rewrite will
